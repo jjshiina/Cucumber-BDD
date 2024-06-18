0d4ebadbbe28af4d9220c423c8efee523739c19f
jj17 <jjshiina17@gmail.com>
2024-06-18 21:12:25 +0800

    Commit 06/18(2)

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/features", glue= {"stepDefinitions"})
+@CucumberOptions(features="src/test/java/features", plugin="json:target/jsonReports/cucumber-reports.json", glue= {"stepDefinitions"})
 
 //, tags = "@deletePlace"
 public class TestRunner {
